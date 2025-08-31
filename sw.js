f15c3b72b00f3e7066d7b8811e3a851cc7a59289
Justin Russell <justin.russell@clublocker.com>
2025-08-31 13:14:51 -0400

    Add basic service worker for offline mode

---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'aupathic-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  'https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap',
+  'https://fonts.gstatic.com/s/quicksand/v31/6xK-dSZaM9iE8KbpRA_LJ3z8mH9BOJvgkP8o58m-wi40.woff2'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - network first, cache fallback
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // If we get a valid response, clone and cache it
+        if (response.status === 200) {
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+        }
+        return response;
+      })
+      .catch(() => {
+        // Network failed, try to get from cache
+        return caches.match(event.request)
+          .then((response) => {
+            if (response) {
+              return response;
+            }
+            // If not in cache and it's a navigation request, return index.html
+            if (event.request.mode === 'navigate') {
+              return caches.match('/index.html');
+            }
+            throw new Error('No cached version available');
+          });
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
